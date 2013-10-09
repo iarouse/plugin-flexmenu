2ebcdd9003786b0fe5e5d9dbcdaa1ec72cf0121e
Sauls <sauls.me@gmail.com>
2013-10-09 14:32:20 +0800

    update for add Less CSS support

--- flexmenu/Example - Advanced/config.rb
+++ flexmenu/Example - Advanced/config.rb
@@ -3,7 +3,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
+sass_dir = "styles"
 images_dir = "images"
 javascripts_dir = "coffee"
 fonts_dir = "fonts"
